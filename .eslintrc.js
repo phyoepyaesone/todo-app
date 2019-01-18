98878e773c9277ad70ef6bfd42a800c86846a0b6
Phyoe Pyae Sone <naylinnoo322@gmail.com>
2019-01-18 13:39:18 +0630

    Eslint allow semi

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     node: true
   },
   extends: 'airbnb-base',
+  
   globals: {
     __static: true
   },
@@ -23,6 +24,7 @@ module.exports = {
     'import/extensions': 0,
     'import/newline-after-import': 0,
     'no-multi-assign': 0,
+    'linebreak-style': 0,
     'semi': ["error", "never"],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
